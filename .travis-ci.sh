d8fa187e803c10380c0196b1b8e92c0af20967aa
Jun Inoue <jun.lambda@gmail.com>
2014-12-11 18:24:54 +0900

    Add Travis CI scripts

---
+++ .travis-ci.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+case $TRAVIS_OS_NAME in
+linux)
+        # We assume Ubuntu.
+        sudo apt-get update -qq
+        sudo apt-get install -qq libsundials-serial-dev ocaml ocaml-native-compilers opam
+       ;;
+osx)
+        brew update
+        brew install sundials ocaml opam
+        ;;
+esac
+
+case $OCAML_MPI in
+yes) OPAM_DEPS="ocamlfind mpi";;
+no)  OPAM_DEPS="ocamlfind";;
+*)   echo "Unrecognized OCAML_MPI: ${OCAML_MPI}"
+     exit 1;;
+esac
+
+opam init
+eval `opam config env`
+# If the system compiler is the right version, try that instead.
+# There are significant differences from opam install <ver>.
+if ! ocaml -version | grep -q $OCAML_VERSION; then
+    opam switch $OCAML_VERSION
+    eval `opam config env`
+    echo "OCaml version (opam)"
+    ocaml -version
+else
+    echo "OCaml version (system)"
+    ocaml -version
+fi
+echo OPAM versions
+opam --version
+opam --git-version
+
+opam install ${OPAM_DEPS}
+./configure
+make
+make -C examples tests.opt.log
